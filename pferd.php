1e6f43194409dd44c873fa85e3e9cb5d93b77b35
Alisa <alisa.lara.grupp@gmail.com>
2019-02-04 13:24:06 +0100

    Links angepasst

--- pferd.php
+++ pferd.php
@@ -67,13 +67,13 @@
           </a>
         </li>
         <li class="nav-item active">
-          <a class="nav-link" href="pferde.php">
+          <a class="nav-link" href="pferd.php">
             <i class="fas fa-fw fa-book"></i>
             <span>Pferde</span>
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="personen.php">
+          <a class="nav-link" href="person.php">
             <i class="fas fa-fw fa-address-book"></i>
             <span>Personen</span>
           </a>
