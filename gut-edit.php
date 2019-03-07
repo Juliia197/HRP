0d536d7b98fcead915639c1365c9f50888ad6494
Kerst1369 <kerstin_hartung1@web.de>
2019-03-07 11:20:37 +0100

    Update gut-edit.php

--- gut-edit.php
+++ gut-edit.php
@@ -171,9 +171,8 @@ if($_SESSION["logged"] == true) {
                   echo "<input class=\"form-control\" type=\"number\" value=\"" . $row_g["einkaufspreis"] . "\" name=\"einkaufspreis\"></div>";
                   echo "
                       <div class=\"form-group\">
-                        <button type=\"submit\" class=\"btn btn-success\">Abschicken</button>
-                        <button class=\"btn btn-secondary\" href=\"gut-edited.php?id_verbrauchsgut=" . $row_g["id_verbrauchsgut"] . "\" role=\"button\">Abbrechen</button>
-                      </div>";
+                        <button type=\"submit\" class=\"btn btn-success\"  href=\"gut-edited.php?id_verbrauchsgut=" . $row_g["id_verbrauchsgut"] . "\" role=\"button\">Abschicken</button>
+                        <a class=\"btn btn-secondary\" href=\"gueter.php\" >Abbrechen</a> </div>";
                 }
               }
               else {
