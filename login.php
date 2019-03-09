f24b7e170c7ddb1cfb58d2dcd6ad9d926663c71d
JuHa <j.harisch@outlook.de>
2019-03-09 18:16:02 +0100

    Allgemein Issues gelöst

--- login.php
+++ login.php
@@ -186,7 +186,7 @@ if (isset($_POST['email'], $_POST['password'])) {
   <meta name="pferdeverwalter" content="" />
   <meta name="JanFreymuth" content="hrp-projekt.de" />
 	
-  <title>Admin Login</title>
+  <title>HRP-Projekt - Login</title>
 
   <!-- Favicon -->
   <link rel="shortcut icon" type="image/icon" href="images/favicon-16x16.png"/>
